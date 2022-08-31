2011e7e6ba315b1e51a89a1fcd5126148670286d
Fomonyuytar-Joseph <josephneymar33@gmail.com>
2022-08-31 20:52:22 +0100

    file module

---
+++ files.js
@@ -0,0 +1,18 @@
+const fs = require('fs')
+//reading files
+ 
+// fs.readFile('./docs/blog1.txt',(err ,data)=>{
+//     if(err){
+//         console.log(err)
+//     }
+//     console.log(data.toString())
+// })
+
+
+//write files
+
+// fs.writeFile('./docs/blog2.txt','Hello WORLD',()=>{
+//     console.log('file was written');
+// })
+
+
