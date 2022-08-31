6dc8ef1d35073d264c49a84665a68b5fecb9adb2
Fomonyuytar-Joseph <josephneymar33@gmail.com>
2022-08-31 16:13:34 +0100

    modules

--- modules.js
+++ modules.js
@@ -1,6 +1,13 @@
-const {youths , ages} = require('./youths')
+const {youths ,ages} = require("./youths")
 
 
 console.log(ages)
 console.log(youths)
 // console.log(youths)
+
+
+
+//os modules
+const os =require('os')
+
+console.log(os.platform() ,os.homedir());
