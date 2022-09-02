6e08059b64ee68bf8d17b97024fd76533a3a0b71
Fomonyuytar-Joseph <josephneymar33@gmail.com>
2022-09-02 15:34:07 +0100

    pass dynamic data

--- app.js
+++ app.js
@@ -10,13 +10,13 @@ app.set("view engine", "ejs");
 app.get("/", (req, res) => {
   // res.send('<p>Home page</p>')
   //   res.sendFile("./views/index.html", { root: __dirname });
-  res.render("index");
+  res.render("index", { title: "Home" });
 });
 
 app.get("/about", (req, res) => {
   // res.send('<p>The About Page</p>')
   //   res.sendFile("./views/about.html", { root: __dirname });
-  res.render("about");
+  res.render("about", { title: "About" });
 });
 
 //redirects
@@ -26,12 +26,12 @@ app.get("/about", (req, res) => {
 // });
 
 app.get("/blogs/create", (req, res) => {
-  res.render("create");
+  res.render("create", { title: "Create a new Blog" });
 });
 
 app.use((req, res) => {
   //   res.status(404).sendFile("./views/404.html", { root: __dirname });
-  res.status(404).render("404");
+  res.status(404).render("404", { title: "404" });
 });
 
 //listen to server
