de07faf4990222ded064c2e77c9e00a70397345e
Fomonyuytar-Joseph <josephneymar33@gmail.com>
2022-09-02 14:03:09 +0100

    setup simple routes

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
-const express = require('express');
-
+const express = require("express");
 
 //express app
-const app =express();
+const app = express();
 
+//listen to request
+app.get("/", (req, res) => {
+  // res.send('<p>Home page</p>')
+  res.sendFile("./views/index.html", { root: __dirname });
+});
 
-app.listen(5000)
+app.get("/about", (req, res) => {
+  // res.send('<p>The About Page</p>')
+  res.sendFile("./views/about.html", { root: __dirname });
+});
+
+//redirects
+
+app.get("/about-us", (req, res) => {
+  res.redirect("/about");
+});
+
+app.use((req, res) => {
+  res.status(404).sendFile("./views/404.html", { root: __dirname });
+});
+
+//listen to server
+app.listen(5000);
